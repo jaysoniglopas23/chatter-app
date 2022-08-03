d638b2d4aa98fa04b71bab961247c4de3a163905
topervillablanca <topervillablanca@gmail.com>
2022-08-03 16:52:44 +0800

    s

--- homes/Happy.js
+++ homes/Happy.js
@@ -1176,7 +1176,7 @@ class Happy extends Component {
           <TouchableOpacity
             style={{
               backgroundColor: '#ECECEC',
-              height: windowHeight / 30,
+              height: windowHeight / 25,
               left: windowWidth / 1.3,
               flexDirection: 'row',
               width: windowWidth / 5 - 5,
