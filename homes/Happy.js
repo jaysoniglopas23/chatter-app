d16a976fe551eefb7ad79e31e032d92a58e9c6d7
jaysoniglopas23 <jaysoniglopas@gmail.com>
2022-08-03 16:45:53 +0800

    a

--- homes/Happy.js
+++ homes/Happy.js
@@ -1176,10 +1176,10 @@ class Happy extends Component {
           <TouchableOpacity
             style={{
               backgroundColor: '#ECECEC',
-              height: windowHeight / 2 -390,
+              height: windowHeight / 2 - 322,
               left: windowWidth / 1.3,
               flexDirection: 'row',
-              width: windowWidth / 5 - 20,
+              width: windowWidth / 5 - 5,
               borderRadius: 2,
               bottom: windowHeight / 2 - 397,
             }}
