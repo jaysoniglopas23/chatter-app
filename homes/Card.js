2d77a3177383e36a23e5d8656722f8c130a90520
topervillablanca <topervillablanca@gmail.com>
2022-08-04 01:44:30 +0800

    s

--- homes/Card.js
+++ homes/Card.js
@@ -211,123 +211,115 @@ class Card extends Component {
             bottom: 300,
             flex: 1,
           }}>
-          <View
+          <TouchableOpacity
             style={{
-              marginTop: windowHeight - 190,
-              height: 150,
-              width: '100%',
-              backgroundColor: '#FFF',
-              borderRadius: 15,
-            }}>
-            <TouchableOpacity
+              width: 110,
+              height: 30,
+              backgroundColor: '#ececec',
+              marginTop: 47,
+              marginLeft: windowWidth / 2 - 10,
+              borderRadius: 3,
+              bottom: windowHeight / 2 -848,
+            }}
+            onPress={() => this.openPhotoAlbum()}>
+            <Svg
+              style={{width: 20, height: 20, left: 10, top: 3}}
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fal"
+              data-icon="angle-left"
+              class="svg-inline--fa fa-angle-left fa-w-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512">
+              <Path
+                fill="black"
+                d="M480 352h-152C323.6 352 320 355.6 320 360s3.582 8 8 8H480c8.822 0 16 7.178 16 16v96c0 8.822-7.178 16-16 16H32c-8.822 0-16-7.178-16-16v-96c0-8.822 7.178-16 16-16h152C188.4 368 192 364.4 192 360S188.4 352 184 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h448c17.67 0 32-14.33 32-32v-96C512 366.3 497.7 352 480 352zM464 432c0-17.6-14.4-32-32-32s-32 14.4-32 32c0 17.6 14.4 32 32 32S464 449.6 464 432zM416 432c0-8.822 7.178-16 16-16s16 7.178 16 16S440.8 448 432 448S416 440.8 416 432zM109.7 165.7L248 27.31V408C248 412.4 251.6 416 256 416s8-3.594 8-8V27.31l138.3 138.3c3.125 3.125 8.188 3.125 11.31 0s3.125-8.188 0-11.31l-152-152c-3.125-3.125-8.188-3.125-11.31 0l-152 152C96.78 155.9 96 157.9 96 160s.7813 4.094 2.344 5.656C101.5 168.8 106.5 168.8 109.7 165.7z"
+              />
+            </Svg>
+            <Text
               style={{
-                width: 110,
+                width: '100%',
                 height: 30,
-                backgroundColor: '#ececec',
-                marginTop: 47,
-                marginLeft: windowWidth / 2 - 10,
-                borderRadius: 3,
-                bottom: windowHeight / 2 - 260,
-              }}
-              onPress={() => this.openPhotoAlbum()}>
-              <Svg
-                style={{width: 20, height: 20, left: 10, top: 3}}
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fal"
-                data-icon="angle-left"
-                class="svg-inline--fa fa-angle-left fa-w-6"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512">
-                <Path
-                  fill="black"
-                  d="M480 352h-152C323.6 352 320 355.6 320 360s3.582 8 8 8H480c8.822 0 16 7.178 16 16v96c0 8.822-7.178 16-16 16H32c-8.822 0-16-7.178-16-16v-96c0-8.822 7.178-16 16-16h152C188.4 368 192 364.4 192 360S188.4 352 184 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h448c17.67 0 32-14.33 32-32v-96C512 366.3 497.7 352 480 352zM464 432c0-17.6-14.4-32-32-32s-32 14.4-32 32c0 17.6 14.4 32 32 32S464 449.6 464 432zM416 432c0-8.822 7.178-16 16-16s16 7.178 16 16S440.8 448 432 448S416 440.8 416 432zM109.7 165.7L248 27.31V408C248 412.4 251.6 416 256 416s8-3.594 8-8V27.31l138.3 138.3c3.125 3.125 8.188 3.125 11.31 0s3.125-8.188 0-11.31l-152-152c-3.125-3.125-8.188-3.125-11.31 0l-152 152C96.78 155.9 96 157.9 96 160s.7813 4.094 2.344 5.656C101.5 168.8 106.5 168.8 109.7 165.7z"
-                />
-              </Svg>
-              <Text
-                style={{
-                  width: '100%',
-                  height: 30,
-                  textAlign: 'center',
-                  lineHeight: 30,
-                  color: 'black',
-                  bottom: 19,
-                  left: 15,
-                }}>
-                アップロード
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => this.goback()}
-              style={{
-                width: 60,
-                height: 30,
-                backgroundColor: '#ececec',
-                marginTop: 25,
-                left: 315,
-                borderRadius: 3,
-                bottom: windowHeight / 2 - 205,
+                textAlign: 'center',
+                lineHeight: 30,
+                color: 'black',
+                bottom: 19,
+                left: 15,
               }}>
-              <Svg
-                style={{width: 20, height: 20, left: 10, top: 3}}
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fal"
-                data-icon="angle-left"
-                class="svg-inline--fa fa-angle-left fa-w-6"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512">
-                <Path
-                  fill="black"
-                  d="M429.3 141.3l-90.51-90.51C326.7 38.74 310.5 32 293.5 32H64C28.65 32 0 60.65 0 96v320c0 35.35 28.65 64 64 64h320c35.35 0 64-28.65 64-64V186.5C448 169.5 441.3 153.3 429.3 141.3zM80 48h213.5C297.1 48 300.6 48.48 304 49.25V152c0 13.23-10.78 24-24 24h-176C90.78 176 80 165.2 80 152V48zM432 416c0 26.47-21.53 48-48 48H64c-26.47 0-48-21.53-48-48V96c0-26.47 21.53-48 48-48v104C64 174.1 81.94 192 104 192h176C302.1 192 320 174.1 320 152V55.98c2.643 1.758 5.137 3.783 7.432 6.078l90.51 90.51C427 161.6 432 173.7 432 186.5V416zM224 240c-48.53 0-88 39.47-88 88S175.5 416 224 416s88-39.47 88-88S272.5 240 224 240zM224 400c-39.69 0-72-32.3-72-72S184.3 256 224 256s72 32.3 72 72S263.7 400 224 400z"
-                />
-              </Svg>
-              <Text
-                style={{
-                  width: '100%',
-                  height: 30,
-                  textAlign: 'center',
-                  lineHeight: 30,
-                  color: 'black',
-                  bottom: 19,
-                  left: 15,
-                }}>
-                保存
-              </Text>
-            </TouchableOpacity>
-          </View>
+              アップロード
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.goback()}
+            style={{
+              width: 60,
+              height: 30,
+              backgroundColor: '#ececec',
+              marginTop: 25,
+              left: 315,
+              borderRadius: 3,
+              bottom: windowHeight / 2 - 793,
+            }}>
+            <Svg
+              style={{width: 20, height: 20, left: 10, top: 3}}
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fal"
+              data-icon="angle-left"
+              class="svg-inline--fa fa-angle-left fa-w-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512">
+              <Path
+                fill="black"
+                d="M429.3 141.3l-90.51-90.51C326.7 38.74 310.5 32 293.5 32H64C28.65 32 0 60.65 0 96v320c0 35.35 28.65 64 64 64h320c35.35 0 64-28.65 64-64V186.5C448 169.5 441.3 153.3 429.3 141.3zM80 48h213.5C297.1 48 300.6 48.48 304 49.25V152c0 13.23-10.78 24-24 24h-176C90.78 176 80 165.2 80 152V48zM432 416c0 26.47-21.53 48-48 48H64c-26.47 0-48-21.53-48-48V96c0-26.47 21.53-48 48-48v104C64 174.1 81.94 192 104 192h176C302.1 192 320 174.1 320 152V55.98c2.643 1.758 5.137 3.783 7.432 6.078l90.51 90.51C427 161.6 432 173.7 432 186.5V416zM224 240c-48.53 0-88 39.47-88 88S175.5 416 224 416s88-39.47 88-88S272.5 240 224 240zM224 400c-39.69 0-72-32.3-72-72S184.3 256 224 256s72 32.3 72 72S263.7 400 224 400z"
+              />
+            </Svg>
+            <Text
+              style={{
+                width: '100%',
+                height: 30,
+                textAlign: 'center',
+                lineHeight: 30,
+                color: 'black',
+                bottom: 19,
+                left: 15,
+              }}>
+              保存
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => this.goBack()}
+            style={{
+              backgroundColor: '#ECECEC',
+              marginHorizontal: 170,
+              height: 31,
+              right: 150,
+              marginBottom: 30,
+              flexDirection: 'row',
+              width: 50,
+              borderRadius: 2,
+              bottom: windowHeight / 2 - 760,
+            }}>
+            <Svg
+              style={{width: 20, height: 30}}
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fal"
+              data-icon="angle-left"
+              class="svg-inline--fa fa-angle-left fa-w-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 192 512">
+              <Path
+                fill="black"
+                d="M25.1 247.5l117.8-116c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L64.7 256l102.2 100.4c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0L25 264.5c-4.6-4.7-4.6-12.3.1-17z"></Path>
+            </Svg>
+            <Text style={{right: 0, top: 6, color: 'black'}}>戻る</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity
-          onPress={() => this.goBack()}
-          style={{
-            backgroundColor: '#ECECEC',
-            marginHorizontal: 170,
-            height: 31,
-            right: 150,
-            marginBottom: 30,
-            flexDirection: 'row',
-            width: 50,
-            borderRadius: 2,
-            bottom: windowHeight / 2 - 305,
-          }}>
-          <Svg
-            style={{width: 20, height: 30}}
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fal"
-            data-icon="angle-left"
-            class="svg-inline--fa fa-angle-left fa-w-6"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 192 512">
-            <Path
-              fill="black"
-              d="M25.1 247.5l117.8-116c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L64.7 256l102.2 100.4c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0L25 264.5c-4.6-4.7-4.6-12.3.1-17z"></Path>
-          </Svg>
-          <Text style={{right: 0, top: 6, color: 'black'}}>戻る</Text>
-        </TouchableOpacity>
       </View>
     );
   }
