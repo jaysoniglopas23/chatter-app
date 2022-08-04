74c892ff722569eefc8aaad7cd56384b377db871
topervillablanca <topervillablanca@gmail.com>
2022-08-04 13:08:18 +0800

    s

--- homes/CoinScreen.js
+++ homes/CoinScreen.js
@@ -1,172 +1,4 @@
-// import React, {Component} from 'react';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
-// import {Text, View, Image, Dimensions} from 'react-native';
-// import {Icon} from 'react-native-vector-icons/Icon';
-// import AntDesign from 'react-native-vector-icons/AntDesign';
-// import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-// import {
-//   Card,
-//   Container,
-//   Interaction,
-//   InteractionText,
-//   InteractionWrapper,
-//   Divider,
-//   Divider1,
-//   Divider2,
-//   Divider3,
-//   Divider4,
-//   InteractionWrapper1,
-//   Interaction1,
-// } from '../styles/CoinStyle';
-// import Svg, {G, Path} from 'react-native-svg';
 
-// const windowWidth = Dimensions.get('window').width;
-
-// class CoinScreen extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {};
-
-//     this.goHome = this.goHome.bind(this);
-//   }
-
-//   goHome() {
-//     this.props.navigation.navigate('home');
-//   }
-
-//   render() {
-//     return (
-//       <View
-//         style={{
-//           backgroundColor: '#FAEA48',
-//           width: '100%',
-//           height: '100%',
-//           flex: 1,
-//         }}>
-//         <InteractionWrapper>
-//           <Image
-//             source={require('../icon/coin13.png')}
-//             style={{
-//               marginLeft: windowWidth / 2 - 10,
-//               width: 70,
-//               height: 70,
-//               resizeMode: 'contain',
-//             }}
-//           />
-//           <Interaction active onPress={() => this.goHome()}>
-//             <Svg
-//               onPress={() => this.goHome()}
-//               style={{width: 20, height: 30, top: 12}}
-//               aria-hidden="true"
-//               focusable="false"
-//               data-prefix="fal"
-//               data-icon="angle-left"
-//               class="svg-inline--fa fa-angle-left fa-w-6"
-//               role="img"
-//               xmlns="http://www.w3.org/2000/svg"
-//               viewBox="0 0 192 512">
-//               <Path
-//                 fill="black"
-//                 d="M25.1 247.5l117.8-116c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L64.7 256l102.2 100.4c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0L25 264.5c-4.6-4.7-4.6-12.3.1-17z"></Path>
-//             </Svg>
-//           </Interaction>
-//         </InteractionWrapper>
-//         <Container>
-//           <Divider>
-//             <Image
-//               source={require('../icon/coin1.png')}
-//               style={{
-//                 alignSelf: 'center',
-//                 width: 200,
-//                 height: 200,
-//                 resizeMode: 'contain',
-//               }}
-//             />
-//           </Divider>
-//           <Divider1>
-//             <Image
-//               source={require('../icon/coin2.png')}
-//               style={{
-//                 marginLeft: windowWidth / 2 - 190,
-//                 width: 80,
-//                 height: 80,
-//                 resizeMode: 'contain',
-//                 bottom: 10,
-//               }}
-//             />
-//           </Divider1>
-//           <Divider2>
-//             <Image
-//               source={require('../icon/coin3.png')}
-//               style={{
-//                 marginLeft: windowWidth / 2 - 190,
-//                 width: 80,
-//                 height: 80,
-//                 resizeMode: 'contain',
-//                 bottom: 10,
-//               }}
-//             />
-//           </Divider2>
-//           {/* <InteractionWrapper1 active>
-//             <Interaction1 active>
-//             <Image
-//               source={require('../icon/coin8.png')}
-//               style={{
-//                 marginLeft: windowWidth / 2 - 190,
-//                 width: 80,
-//                 height: 40,
-//                 resizeMode: 'contain',
-//                 bottom: 0,
-//               }}
-//             />
-//             </Interaction1>
-//           </InteractionWrapper1> */}
-//           <Divider1>
-//             <Image
-//               source={require('../icon/coin4.png')}
-//               style={{
-//                 marginLeft: windowWidth / 2 - 190,
-//                 width: 120,
-//                 height: 120,
-//                 resizeMode: 'contain',
-//                 bottom: 30,
-//               }}
-//             />
-//           </Divider1>
-//           <Image
-//             source={require('../icon/coin5.png')}
-//             style={{
-//               marginLeft: windowWidth / 2 - 410,
-//               width: 120,
-//               height: 60,
-//               resizeMode: 'contain',
-//               bottom: 4,
-//             }}
-//           />
-//           {/* <InteractionWrapper1 active>
-//             <Interaction1 active>
-//             </Interaction1>
-//           </InteractionWrapper1> */}
-//           <Divider1>
-//             <Image
-//               source={require('../icon/coin6.png')}
-//               style={{
-//                 marginLeft: windowWidth / 2 - 190,
-//                 width: 120,
-//                 height: 120,
-//                 resizeMode: 'contain',
-//                 bottom: 30,
-//               }}
-//             />
-//           </Divider1>
-//         </Container>
-//       </View>
-//     );
-//   }
-// }
-
-// export default CoinScreen;
 
 import React, {Component} from 'react';
 
@@ -302,15 +134,14 @@ class CoinScreen extends Component {
           let self = this;
 
           global.socket.off('emit-buy-points-bundle');
-          alert(ret);
+          // alert(ret);
 
           global.bundle = ret;
         });
-        let params = {
-          uuid: global.deviceid,
-          requestor: global.user_id,
-          receipt: purchase.transactionReceipt,
-        };
+        let params = {};
+
+        params['amount'] = global.price;
+
 
         global.socket.emit('on-buy-points-bundle', params);
 
@@ -339,8 +170,12 @@ class CoinScreen extends Component {
       RNIap.getProducts(self.state.items).then(res => {
         self.setState({
           res: res,
+          price: res[0].price,
         });
-        // alert(JSON.stringify(res));
+
+        global.price = self.state.price;
+
+        alert(JSON.stringify(global.price));
         this.getCoins();
       });
     });
@@ -411,12 +246,19 @@ class CoinScreen extends Component {
 
       self.setState({
         bundle: ret,
+        amount:ret[0].amount,
         // showLoading: false,
-        bundleid: ret.bundleid,
+        bundleid: ret[0].bundleid,
+        description:ret[0].description,
       });
+
+      global.amount = self.state.amount;
+
+     
     });
 
     let params = {bundleid: global.bundleid};
+  
 
     global.socket.emit('on-points-bundle', params);
   }
