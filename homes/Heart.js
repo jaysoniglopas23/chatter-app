a64902e8016d8e598da0a67c1e4e8ab4fd559e8f
topervillablanca <topervillablanca@gmail.com>
2022-08-05 10:51:32 +0800

    s

--- homes/Heart.js
+++ homes/Heart.js
@@ -195,7 +195,7 @@ class Heart extends Component {
             height: windowHeight / 13,
             backgroundColor:'black',
             width: '100%',
-            top: windowHeight / 2 - 800,
+            top: windowWidth / 2 - 300,
           }}>
           <TouchableOpacity
             onPress={() => this.goBack()}
