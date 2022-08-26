4973a64f47fd408ec48a650c1395fa9f4f293da6
topervillablanca <topervillablanca@gmail.com>
2022-08-26 13:56:02 +0800

    sds

--- homes/News.js
+++ homes/News.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity, Image,Dimensions} from 'react-native';
+import {Text, View, TouchableOpacity, Image, Dimensions} from 'react-native';
 import {createStackNavigator, HeaderTitle} from '@react-navigation/stack';
 import {CardStyleInterpolators} from '@react-navigation/stack';
 import Svg, {G, Path} from 'react-native-svg';
@@ -164,36 +164,44 @@ class News extends Component {
             </Svg>
           </TouchableOpacity>
         </View>
-        <View style={{top: windowWidth - 20, backgroundColor: '#ffff', flex: 1,width:'100%' ,height:windowHeight}}>
-        <TouchableOpacity
-          onPress={() => this.goBack()}
+        <View
           style={{
-            backgroundColor: '#ECECEC',
-            marginHorizontal: 170,
-            height: 31,
-            right: 150,
-            marginBottom: 30,
-            flexDirection: 'row',
-            width: 50,
-            borderRadius: 2,
-            bottom: windowHeight / 2 - 415,
+            top: windowWidth - 35,
+            backgroundColor: '#ffff',
+            // flex: 1,
+            width: '100%',
+            height: windowHeight / 2 -550,
+            // backgroundColor: 'black',
           }}>
-          <Svg
-            style={{width: 20, height: 30}}
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fal"
-            data-icon="angle-left"
-            class="svg-inline--fa fa-angle-left fa-w-6"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 192 512">
-            <Path
-              fill="black"
-              d="M25.1 247.5l117.8-116c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L64.7 256l102.2 100.4c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0L25 264.5c-4.6-4.7-4.6-12.3.1-17z"></Path>
-          </Svg>
-          <Text style={{right: 0, top: 6, color: 'black'}}>戻る</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => this.goBack()}
+            style={{
+              backgroundColor: '#ECECEC',
+              marginHorizontal: 170,
+              height: 31,
+              right: 150,
+              marginBottom: 30,
+              flexDirection: 'row',
+              width: 50,
+              borderRadius: 2,
+              bottom: windowHeight / 2 - 415,
+            }}>
+            <Svg
+              style={{width: 20, height: 30}}
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fal"
+              data-icon="angle-left"
+              class="svg-inline--fa fa-angle-left fa-w-6"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 192 512">
+              <Path
+                fill="black"
+                d="M25.1 247.5l117.8-116c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L64.7 256l102.2 100.4c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0L25 264.5c-4.6-4.7-4.6-12.3.1-17z"></Path>
+            </Svg>
+            <Text style={{right: 0, top: 6, color: 'black'}}>戻る</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
