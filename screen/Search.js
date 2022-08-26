a7d413d16f143e89ffbc77f14b56c41a3c67fb91
topervillablanca <topervillablanca@gmail.com>
2022-08-26 15:44:52 +0800

    sds

--- screen/Search.js
+++ screen/Search.js
@@ -456,7 +456,7 @@ export default class Search extends Component {
               fontSize: 9,
               width: '21%',
               bottom: 68,
-              left: "43%",
+              left: 165,
               backgroundColor: '#fff',
               color: 'black',
             }}>
