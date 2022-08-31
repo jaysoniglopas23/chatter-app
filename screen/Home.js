f2e80c98d765f0ac0cb68e2bb9b1909038062d64
jaysoniglopas23 <jaysoniglopas@gmail.com>
2022-08-31 10:29:51 +0800

    sd

--- screen/Home.js
+++ screen/Home.js
@@ -365,7 +365,7 @@ class DisplayAnImage extends Component {
 
         <View
           style={{
-            flex: 0.6,
+            flex: 0.8,
             justifyContent: 'center',
             flexDirection: 'row',
             backgroundColor: 'white',
