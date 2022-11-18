1a3589c43e715ff384b71cc83bfbe649d3550960
jaysoniglopas23 <jaysoniglopas23>
2022-11-18 16:34:07 +0800

    call

--- screen/caller.js
+++ screen/caller.js
@@ -1,116 +1,8 @@
-// import React, {useRef, Component} from 'react';
 
-// import {
-//   Easing,
-//   ActivityIndicator,
-//   Image,
-//   StyleSheet,
-//   Text,
-//   View,
-//   Animated,
-//   NativeModules,
-//   Platform,
-//   Dimensions,
-//   TouchableOpacity,
-//   TouchableWithoutFeedback,
-//   FlatList,
-//   Modal,
-//   TextInput,
-//   KeyboardAvoidingView,
-//   findNodeHandle,
-// } from 'react-native';
-
-// import Svg, {G, Path} from 'react-native-svg';
-
-// const windowWidth = Dimensions.get('window').width;
-// const windowHeight = Dimensions.get('window').height;
-
-// const twilioVideo = React.createRef();
-
-// class Caller extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       accessToken: null,
-//       trackSid: null,
-//       trackIdentifier: null,
-//       messageText: global.locale == 'en' ? ' Connecting...' : '接続する...',
-//       usernameText: global.name,
-//       callStatus: 0,
-//       videoOn: false,
-//       calleeVideoOn: false,
-//       mute: false,
-//       speakerOn: false,
-//     };
-
-//   }
-
-//   componentDidMount() {
-
-//       global.socket.on('emit-someone-is-calling', function (ret) {
-//         global.socket.off('emit-someone-is-calling');
-
-//         // alert(2222)
-
-//       });
-
-//       let params = {};
-//       params['nickname'] = global.nickname;
-//       params['from'] = global.myid;
-//       params['to_id'] =global.otherid;
-
-//       // alert(111);
-
-//       global.socket.emit('on-audio-call', params);
-
-//   }
-
-//   back() {
-
-//     global.socket.on('emit-drop-callee-audio-call', function (ret) {
-//       global.socket.off('emit-drop-callee-audio-call');
-
-//       // alert(2222)
-
-//     });
-
-//     let params = {};
-//     // params['socketid'] = this.state.socketid;
-//     params['from'] = global.myid;
-//     params['to'] =global.otherid;
-
-//     this.props.navigation.navigate('Chat');
-
-//     global.socket.emit('on-drop-caller-audio-call', params);
-//   }
-
-//   render() {
-//     return (
-//       <View
-//         styles={{
-//           width: windowWidth,
-//           height: windowHeight,
-//           flexDirection: 'column',
-//           backroundColor:'black'
-//         }}>
-
-//           <View style={{alignSelf:'center', backroundColor:'red',width:50,height:50,top:100}}>
-//             <TouchableOpacity  onPress={() => this.back()}>
-//               <Text>Drop</Text>
-//             </TouchableOpacity>
-//           </View>
-
-//       </View>
-//     );
-//   }
-// }
-
-// export default Caller;
 
 import React, {useRef, Component} from 'react';
 import ZegoUIKitPreBuildCall, {
-  ONE_ON_ONE_VIDEO_CALL_CONFIG,
+  ONE_ON_ONE_VIDEO_CALL_CONFIG,ONE_ON_ONE_VOICE_CALL_CONFIG 
 } from '@zegocloud/zego-uikit-prebuilt-call-rn';
 import {ZegoLayoutMode} from '@zegocloud/zego-uikit-rn';
 import {View,Text,Dimensions,TouchableOpacity} from 'react-native';
@@ -192,16 +84,8 @@ class Callee extends Component {
           userName={this.state.username}
           callID="testidcall"
           config={{
-            ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
-            onOnlySelfInRoom: () => {
-              this.props.navigation.navigate('Chat');
-      
-            },
-            onHangUp: () => {
-              this.props.navigation.navigate('Chat');
-            },
-            //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/'bo
-    
+            // ...ONE_ON_ONE_VIDEO_CALL_CONFIG,
+            ...ONE_ON_ONE_VOICE_CALL_CONFIG ,
             layout: {
               mode: ZegoLayoutMode.pictureInPicture,
               config: {
@@ -209,12 +93,23 @@ class Callee extends Component {
                 isSmallViewDraggable: true,
                 // switchLargeOrSmallViewByClick: true,
                 turnOnCameraWhenJoining: false,
-                // turnOnMicrophoneWhenJoining: false,
+                turnOnMicrophoneWhenJoining: false,
+                
                 // useSpeakerWhenJoining: true,
                
               },
             },
-            ///\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+            
+            onOnlySelfInRoom: () => {
+              this.props.navigation.navigate('Chat');
+      
+            },
+            onHangUp: () => {
+              this.props.navigation.navigate('Chat');
+            },
+          
+         
+          
           }}
         />
       </View>
